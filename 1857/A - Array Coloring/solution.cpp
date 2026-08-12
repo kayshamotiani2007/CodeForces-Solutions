f14c06e7787e9c72379c152f38f3974413c2ab66
Kaysha Motiani <kayshamotiani2007@gmail.com>
2026-08-13 00:01:20 +0530

    Add Array Coloring [A] from Codeforces

--- 1857/A - Array Coloring/solution.cpp
+++ 1857/A - Array Coloring/solution.cpp
@@ -1,39 +1,34 @@
-#include <iostream>
-#include <vector>
- 
+#include <bits/stdc++.h>
 using namespace std;
  
 void solve() {
     int n;
     cin >> n;
+    int odd_count=0;
     
-    int odd_count = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i=0 ; i<n ; i++) {
         int x;
         cin >> x;
-        if (x % 2 != 0) {
-            odd_count++;
+        
+        if(x%2!=0) {
+            odd_count++ ;
         }
+        
     }
-    
-    if (odd_count % 2 == 0) {
+    if (odd_count%2==0) {
         cout << "YES
-";
-    } else {
+" ;
+    }
+    else {
         cout << "NO
-";
+" ;
     }
 }
- 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
     int t;
     cin >> t;
-    while (t--) {
+    while(t--) {
         solve();
     }
-    
     return 0;
 }
