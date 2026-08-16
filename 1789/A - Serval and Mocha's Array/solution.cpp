75031d75519a87cc694477782ab32d092dca8c11
Kaysha Motiani <kayshamotiani2007@gmail.com>
2026-08-16 13:30:04 +0530

    Add Serval and Mocha's Array [A] from Codeforces

---
+++ 1789/A - Serval and Mocha's Array/solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+   
+    
+    
+    for (int i=0 ; i<n ; i++) {
+        cin >> arr[i];
+    }
+    
+    for ( int i=0 ; i<n ; i++) {
+        for ( int j=i+1 ; j<n ; j++) {
+            if(gcd(arr[i] , arr[j]) <= 2) {
+                cout << "YES
+" ;
+                return ;
+            }
+            
+        }
+    }
+    cout << "NO
+" ;
+    
+}
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        solve() ;
+    }
+    return 0;
+ 
+}
